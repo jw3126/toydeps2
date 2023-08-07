a136a6ab7c18b82bbb229279aaa7ba3c8e6db4b7
Jan Weidner <jw3126@gmail.com>
2023-08-07 09:10:01 +0200

    f

---
+++ use_deps.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Unpack deps"
+
+tar -xzf ~/deps.tar.gz -C ~/deps
+mkdir ~/.julia
+mv ~/deps/artifacts  ~/.julia/artifacts
+mv ~/deps/compiled   ~/.julia/compiled
+mv ~/deps/packages   ~/.julia/packages
+mv ~/deps/registries ~/.julia/registries
+mv ~/deps/julia      ~/julia
+
+echo "Running julia"
+juila -e 'using Pkg; Pkg.add("PyCall"); using PyCall'
