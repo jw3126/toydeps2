a639635a0163c6f71cd2baffac061bd7fb320b81
Jan Weidner <jw3126@gmail.com>
2023-08-07 14:03:10 +0200

    f

--- use_deps.sh
+++ use_deps.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-echo pwd
-pwd
-echo ls
-ls
-echo ls deps
-ls deps
+mkdir ~/.julia
+mv deps/artifacts  ~/.julia/artifacts
+mv deps/compiled   ~/.julia/compiled
+mv deps/packages   ~/.julia/packages
+mv deps/registries ~/.julia/registries
+mv deps/julia      ~/julia
+export PATH=~/julia/bin:$PATH
 
-# mkdir ~/.julia
-# mv ~/deps/artifacts  ~/.julia/artifacts
-# mv ~/deps/compiled   ~/.julia/compiled
-# mv ~/deps/packages   ~/.julia/packages
-# mv ~/deps/registries ~/.julia/registries
-# mv ~/deps/julia      ~/julia
-
-# echo "Running julia"
-# juila -e 'using Pkg; Pkg.add("PyCall"); using PyCall'
+echo "Running julia"
+juila -e 'using Pkg; Pkg.add("PyCall"); using PyCall'
